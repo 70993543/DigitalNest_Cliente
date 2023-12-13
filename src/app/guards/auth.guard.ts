2a3e292373e3a03f42a0b8fb68125fc24d7e18c5
Bryan Mancilla <70993543@certus.edu.pe>
2023-12-12 19:02:15 -0500

    Integración de guards

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { ClienteService } from "../services/cliente.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+  constructor(
+    private _clienteService: ClienteService,
+    private _router: Router,
+  ){
+
+  }
+
+  canActivate():any{
+    if (!this._clienteService.isAuthenticated()) {
+      this._router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
+}
